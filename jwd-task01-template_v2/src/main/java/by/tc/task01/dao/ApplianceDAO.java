809cbbdaad09829377d722f46c958e6b3d6b54f9
Andrew <alisov3102@gmail.com>
2022-12-04 12:03:57 +0300

    initial

---
+++ jwd-task01-template_v2/src/main/java/by/tc/task01/dao/ApplianceDAO.java
@@ -0,0 +1,10 @@
+package by.tc.task01.dao;
+
+import java.util.ArrayList;
+
+
+public interface ApplianceDAO {
+
+	ArrayList<ArrayList<String>> find(String groupName);
+
+}
---
+++ jwd-task01-template_v2/src/main/java/by/tc/task01/service/ApplianceService.java
@@ -0,0 +1,14 @@
+package by.tc.task01.service;
+
+import java.util.ArrayList;
+
+import by.tc.task01.entity.Appliance;
+import by.tc.task01.entity.criteria.Criteria;
+
+public interface ApplianceService {
+
+	ArrayList<Appliance> find(Criteria criteria);
+
+	Appliance findLowestCostAppliance();
+
+}
